3ae2cb302d67885c851ef4a5d8f78d39f3b36ee4
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-03-26 15:22:01 -0400

    heroku launch

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PharmaHacks:app
--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, redirect, render_template, request, url_for
 
+app = Flask(__name__, template_folder="template", static_folder = "static")
+
 @app.route('/', methods = ["GET"])
 def index():
-    return 
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()

8e72fa26e33e02a03c92ddb7ffde0c2d2e439498
Owen Glahn <owen.glahn@mail.mcgill.ca>
2022-03-26 15:27:19 -0400

    debug true, remove static

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__, template_folder="template", static_folder = "static")
+app = Flask(__name__, template_folder="template")
 
 @app.route('/', methods = ["GET"])
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
